80f10865d39a36995b59ca64064d62bebcd1070b
Jānis Džeriņš <jonis@latnet.lv>
2009-10-23 06:35:20 +0000

    Added XCVB support (patch from François-René Rideau <fahree@gmail.com>).

--- rfc2388.lisp
+++ rfc2388.lisp
@@ -21,6 +21,8 @@
 ;;;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 ;;;; THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#+xcvb (module (:depends-on ("packages")))
+
 (in-package :rfc2388)
 
 
